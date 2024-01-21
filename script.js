e59282fecccbd35030eff01ba685c9d82ddf382c
Mohithreddy106 <mohithreddy106@gmail.com>
2024-01-21 13:29:28 +0530

    Added some prompt

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function onClick() {
-  
+  // Game Time
   let name = prompt("Please Enter your name: ");
   let gameNum = name.length;
   alert(`Hello ${name.toUpperCase()} ! Welcome to the Number guessing game :)`);
-  let userNum = Math.round(prompt(` Guess the Game Number : )`));
+  let userNum = Math.round(prompt(` Its Game Time ! Guess the Game Number : )`));
 
   while (userNum !== gameNum) {
     userNum = Math.round(
