2d2fe8bcd61397efe426b53a360b3c3c6eda5af0
Mohithreddy106 <mohithreddy106@gmail.com>
2024-01-21 21:55:20 +0530

    add background

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
 function onClick() {
   // Game Time
-  
   let name = prompt("Please Enter your name: ");
   let gameNum = name.length;
   alert(`Hello ${name.toUpperCase()} ! Welcome to the Number guessing game :)`);
-  let userNum = Math.round(prompt(` Its Game Time ! Guess the Game Number : )`));
+  let userNum = Math.round(prompt(` It's Game Time! Guess the Game Number : )`));
 
   while (userNum !== gameNum) {
     userNum = Math.round(
-      prompt(`Badluck ${name.toUpperCase()}, Your guess is wrong. Guess again:\n(Hint: Answer is in Your Name)`)
+      prompt(`Bad luck ${name.toUpperCase()}, Your guess is wrong. Guess again:\n(Hint: Answer is in Your Name)`)
     );
   }
 
-  document.write(`Congratulations ${name.toUpperCase()}, your Guess is Correct! Great Job`);
-  
+  document.querySelector('.opt').textContent = `Congratulations ${name.toUpperCase()}, Your Guess is Correct! Great Job`;
 }
